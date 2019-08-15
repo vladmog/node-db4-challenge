fbf20077ad1656a97012862e5741964906183ac6
vladmog <vladmog@gmail.com>
2019-08-15 15:38:15 -0700

    Created unit fields in third table before migration

--- data/migrations/20190815141918_recipes_tables.js
+++ data/migrations/20190815141918_recipes_tables.js
@@ -23,25 +23,31 @@ exports.up = function(knex) {
 
     //Recipe ingredients table
     .createTable('recipe_ingredients', tbl => {
-      tbl.increments();
-
-      tbl
-        .integer('recipe_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('recipes')
-        .onDelete('RESTRICT')
-        .onUpdate('CASCADE'); // if the PK on primary table changes, what to do?
-      tbl
-        .integer('ingedient_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('ingredients')
-        .onDelete('RESTRICT')
-        .onUpdate('CASCADE'); // if the PK on primary table changes, what to do?
+        tbl.increments();
+        tbl
+            .integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE'); // if the PK on primary table changes, what to do?
+        tbl
+            .integer('ingedient_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('ingredients')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE'); // if the PK on primary table changes, what to do?
+        tbl
+            .string('ingredient_unit')
+            .notNullable()
+        tbl
+            .real('ingredient_units')
+            .notNullable()
     });
+
 };
 
 exports.down = function(knex) {
