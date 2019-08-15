8ab4c23d9c0382d1951c1adfd60cb42d0a786b07
vladmog <vladmog@gmail.com>
2019-08-15 15:37:41 -0700

    WIP Created table in migrations

---
+++ data/migrations/20190815141918_recipes_tables.js
@@ -0,0 +1,52 @@
+
+exports.up = function(knex) {
+    return knex.schema
+
+    //Ingredients table
+    .createTable('ingredients', tbl => {
+      tbl.increments();
+      tbl
+        .string('ingredient_name', 255)
+        .notNullable()
+        .unique();
+    })
+
+    //Recipes table
+    .createTable('recipes', tbl => {
+      tbl.increments();
+      tbl
+        .string('recipe_name', 255)
+        .notNullable()
+        .unique();
+    })
+    
+
+    //Recipe ingredients table
+    .createTable('recipe_ingredients', tbl => {
+      tbl.increments();
+
+      tbl
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE'); // if the PK on primary table changes, what to do?
+      tbl
+        .integer('ingedient_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE'); // if the PK on primary table changes, what to do?
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('ingredients')
+};
